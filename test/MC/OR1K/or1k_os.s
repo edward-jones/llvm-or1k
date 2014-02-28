35db15bf28eadbfa1c95b5694167c2971ded5da3
Michele Beretta <michele3.beretta@mail.polimi.it>
2014-02-28 22:33:23 +0100

    Added descripton of instruction for OS interaction

---
+++ test/MC/OR1K/or1k_os.s
@@ -0,0 +1,10 @@
+# RUN: llvm-mc -arch=or1k --show-encoding %s | FileCheck %s
+
+    l.sys 42
+# CHECK: # encoding: [0x20,0x00,0x00,0x2a]
+
+	l.trap 42
+# CHECK: # encoding: [0x21,0x00,0x00,0x2a]
+
+	l.rfe
+# CHECK: # encoding: [0x90,0x00,0x00,0x00]
